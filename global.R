8dcf766c08e967e9bc4f698e7db091d8b812570d
epspi <eugene@ahalogy.com>
2016-04-21 14:29:57 -0400

    write.table replacing write.cv

--- global.R
+++ global.R
@@ -255,7 +255,7 @@ rescrape <- function( use.progress = T ) {
 
     # Add new timestamp
     data.frame( time = Sys.time(), method = ifelse( use.progress, "browser", "cron" ) ) %>%
-        write.csv( "CSV/timestamp.csv", append = T, row.names = F )
+        write.table( "CSV/timestamp.csv", append = T, sep = ",", row.names = F )
 
     write.csv(auctions_df, "CSV/auctions.csv", row.names = F)
     write.csv(items_df, "CSV/items.csv", row.names = F)
